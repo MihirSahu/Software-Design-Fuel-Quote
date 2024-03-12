fc69732c360d9a55c0ab217295f2bdd2bc0cdbcd
Mihir Sahu <2002mihir@gmail.com>
2024-03-12 17:34:54 -0500

    feat: add protection to pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
         message: 'You have been logged in!',
         color: 'teal',
       });
-      push('/private')
+      push('/quote')
     }
     else {
       const error = await response.json();
